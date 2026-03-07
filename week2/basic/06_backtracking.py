d1122801f50d3b34d34bf10281a6a49dc24d89ad
kim8796 <kim8796@gmail.com>
2026-03-07 22:39:48 +0900

    06

--- week2/basic/06_backtracking.py
+++ week2/basic/06_backtracking.py
@@ -34,7 +34,7 @@ def combinations(n, k):
         모든 조합의 리스트
     """
     result = []
-    
+
     def backtrack(start, current_combination):
         """
         백트랙킹 헬퍼 함수
@@ -43,15 +43,26 @@ def combinations(n, k):
             start: 탐색을 시작할 숫자
             current_combination: 현재까지 선택한 숫자들
         """
+        # print(f"current combination : {current_combination}")
         # TODO: base case - k개를 모두 선택했으면 결과에 추가
-        pass
         
+        if len(current_combination) == k:
+            result.append(current_combination.copy()) 
+            return 
+
         # TODO: start부터 n까지 숫자를 하나씩 시도
+        
+
+        for i in range(start,n+1):
+            current_combination.append(i)
+            backtrack(i+1,current_combination)
+            current_combination.pop()
+            
         ## TODO: 백트랙킹 3단계 구현
         ## 1. 선택(Choose)
         ## 2. 탐색(Explore)
         ## 3. 취소(Unchoose)
-        pass
+    
     
     backtrack(1, [])
     return result
