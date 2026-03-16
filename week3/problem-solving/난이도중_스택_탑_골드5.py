81329fa8d002524aee86547a9236625a65a3934b
김용 <kim8796@gmail.com>
2026-03-16 22:49:33 +0900

    세수의합, 탑
    세수의 합 a+b+c = d => a+b = d -c 로 생각전환이 필요함  d를 내림차순으로 이동시키며 set을 이용해서 a+b랑 같은  값이 있는걸 찾아내면 답
    탑
    O(n^2)방식으로 먼저 풀고 시간초과로 스택을 사용. 스택 사용시 왼쪽부터 이동하며 기준 탑 보다 더 작으면 팝하고 스택 꼭대기에 있는 남은 탑이 수신탑임 추후 이 탑을   튜플형태로 스택에 추가함 다음 기준에서 수신가능할수 있어서

--- week3/problem-solving/난이도중_스택_탑_골드5.py
+++ week3/problem-solving/난이도중_스택_탑_골드5.py
@@ -1,2 +1,38 @@
 # 스택 - 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2493
+
+
+import sys
+
+def main():
+    top = int(sys.stdin.readline())
+    t_num = list(map(int,sys.stdin.readline().split()))
+    res = [0]*top
+    stack = []
+
+    for i in range(top) :
+        while stack and stack[-1][1] < t_num[i]:
+            stack.pop()
+        
+        if stack : 
+            res[i] = stack[-1][0] + 1
+        stack.append((i,t_num[i]))
+
+    print(*res)
+
+
+  
+    # for i in range(top-1,0,-1):
+    #     j = i-1
+    #     while j >= 0 :
+    #         if t_num[i]<=t_num[j]:
+    #             res[i] = j+1
+    #             j-=1
+    #             break
+    #         else: 
+    #             j-=1
+
+    # print(res)
+    # return res
+if __name__ == "__main__":
+    main()
