91541a256d18d003c8ae9e5124f881b0e22aa047
김용 <kim8796@gmail.com>
2026-03-17 21:48:45 +0900

    reverseNodesinkGroup
    리스트 뒤집는 방법이 어렵다
    nxt = curr.next
    curr.next = prev
    prev = curr
    curr = nxt
    여기 반복이 핵심
    그 뒤에는 prev로 더미 위치의 연결을 뒤집힌 노드로 옮겨주고  prev를 다시 다음 시작점의 뒤 노드로 보냄

--- week3/problem-solving/Extra_링크드리스트_ReverseNodesinkGroup.py
+++ week3/problem-solving/Extra_링크드리스트_ReverseNodesinkGroup.py
@@ -1,2 +1,29 @@
 # 링크드리스트 - Reverse Nodes in k-Group
 # 문제 링크: https://leetcode.com/problems/reverse-nodes-in-k-group/description/?envType=study-plan-v2&envId=top-interview-150
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        dummy = ListNode(0, head)
+        group_prev = dummy
+
+        while True:
+            kth = group_prev
+            for _ in range(k):
+                kth = kth.next
+                if not kth:
+                    return dummy.next
+
+            group_next = kth.next
+
+            prev = group_next
+            curr = group_prev.next
+
+            while curr != group_next:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+
+            temp = group_prev.next   
+            group_prev.next = kth
+            group_prev = temp
