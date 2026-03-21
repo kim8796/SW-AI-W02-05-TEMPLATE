9efce802475571c63c3d87c778ffa6f9d5aac474
김용 <kim8796@gmail.com>
2026-03-21 22:22:12 +0900

    상근이의여행
    노드의 수를 N으로 하면 연결그래프에서 모든 정점을 방문하는 최소 간선수는 N-1 이고 최단 루트 => 스패닝트리 => 최소간선수 라서 답은 뭘해도 N-1

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,21 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+
+# 첫줄 테스트케이스 수 (결과값 갯수)
+
+
+import sys
+from collections import deque
+
+def main():
+    res_count = int(sys.stdin.readline())
+    for _ in range(res_count):
+        f,m = map(int,sys.stdin.readline().split())
+        flight = [tuple(map(int,sys.stdin.readline().split())) for _ in range(m)]
+        print(f-1)
+
+
+
+
+if __name__ == "__main__" :
+    main()
