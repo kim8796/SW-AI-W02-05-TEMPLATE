229386c950de67061de3e2e3693d5d78f94a8d4a
김용 <kim8796@gmail.com>
2026-03-30 14:47:34 +0900

    feat: add coin problem template

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,24 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+import sys
+
+
+def main():
+    input = sys.stdin.readline
+
+    t = int(input().strip())
+
+    for _ in range(t):
+        n = int(input().strip())
+        coins = list(map(int, input().split()))
+        target = int(input().strip())
+
+        
+        # TODO: solve(n, coins, target) 작성 후 결과 출력
+        # answer = solve(n, coins, target)
+        # print(answer)
+
+
+if __name__ == "__main__":
+    main()
